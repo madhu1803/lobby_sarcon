aa925ddf3a66175dfb375b090d6445b442eb631a
madhumithaa <madhumithaa1411@gmail.com>
2020-06-08 08:53:49 +0530

    Addded slick carousel only in expo hall

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,3 +36,37 @@ $(document).ready(function () {
     }
 
 });
+
+//on resizing window adjust slick carousel
+
+window.onresize = function (event) {
+    width = $(window).width()
+    if (width >= 992) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+
+    } else if (width <= 576) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 4,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+        });
+    } else if (width >= 768 || width < 576) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+        });
+    }
+};
