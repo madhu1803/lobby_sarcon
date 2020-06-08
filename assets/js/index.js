dc8f730933f42fa64c8711b2081d4801bbd7dc84
madhumithaa <madhumithaa1411@gmail.com>
2020-06-08 08:31:17 +0530

    slick caroousel media queries done

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,35 @@ AOS.init();
 //slick caorousel
 
 $(document).ready(function () {
-    $(".multiple-items").slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
+
+    width = $(window).width()
+    if (width >= 992) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+
+    } else if (width <= 576) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 4,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+        });
+    } else if (width >= 768 || width < 576) {
+        $(".multiple-items").slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+        });
+    }
+
 });
