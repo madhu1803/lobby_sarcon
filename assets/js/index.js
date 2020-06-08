32644c69d2db29acb6063e83d8e9caa44107bd39
madhumithaa <madhumithaa1411@gmail.com>
2020-06-08 14:32:05 +0530

    padding should be adjusted

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
             infinite: true,
             slidesToShow: 4,
             slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            // autoplay: true,
+            // autoplaySpeed: 2000,
         });
 
     } else if (width <= 576) {
@@ -20,8 +20,8 @@ $(document).ready(function () {
             infinite: true,
             slidesToShow: 2,
             slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            // autoplay: true,
+            // autoplaySpeed: 2000,
 
         });
     } else if (width >= 768 && width > 576) {
@@ -29,8 +29,8 @@ $(document).ready(function () {
             infinite: true,
             slidesToShow: 3,
             slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            // autoplay: true,
+            // autoplaySpeed: 2000,
 
         });
     }
