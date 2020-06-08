6da79fff36a8b0e2259657735b858bd3ad65c0d0
madhumithaa <madhumithaa1411@gmail.com>
2020-06-08 08:10:25 +0530

    Added slick carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,14 @@
 // aos init
 AOS.init();
+
+//slick caorousel
+
+$(document).ready(function () {
+    $(".multiple-items").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
