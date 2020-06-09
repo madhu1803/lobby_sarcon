a9c3c74bc87ca9d20ef4a0ce7f640735820e8c45
madhumithaa <madhumithaa1411@gmail.com>
2020-06-09 23:10:05 +0530

    carousel rows done alignment to be fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,50 +5,51 @@ AOS.init();
 
 $(document).ready(function () {
 
-    width = $(window).width()
-    if (width >= 992) {
-        $(".multiple-items").slick({
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
-        });
+  width = $(window).width()
+  if (width >= 992) {
+    $(".multiple-items").slick({
+      rows: 2,
+      slidesPerRow: 3,
+      infinite: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
 
-    } else if (width <= 576) {
-        $(".multiple-items").slick({
-            infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+  } else if (width <= 576) {
+    $(".multiple-items").slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 3000,
 
-        });
-    } else if (width >= 768 && width > 576) {
-        $(".multiple-items").slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+    });
+  } else if (width >= 768 && width > 576) {
+    $(".multiple-items").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 3000,
 
-        });
-    }
+    });
+  }
 
 });
 
 // Event Modal 
 //close event modal
 function eventmodal() {
-    $("#eventmodal").toggleClass("d-none");
+  $("#eventmodal").toggleClass("d-none");
 }
 //btn click
 $("#events-modal-btn").click(function () {
-    eventmodal();
+  eventmodal();
 });
 
 $(".tab-item").click(function () {
-    $("#eventmodal").addClass("d-none");
+  $("#eventmodal").addClass("d-none");
 });
 //on resizing window adjust slick carousel
 
