5dd5f534171b111d812078775cbb94f08c34b48a
madhumithaa <madhumithaa1411@gmail.com>
2020-06-16 14:21:40 +0530

    cleaned up js code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 // aos init
 AOS.init();
+
+//close announcemenets modal on screen ready
 toggleAnnouncementsModal();
 
 // dropdown profile
@@ -27,111 +29,6 @@ $(function () {
       });
   });
 });
-
-//Business card modal
-function openBusinessCardModal() {
-  $("#businessCardModal").modal("show");
-}
-
-// Notification Modal
-// minimize modal
-$("#minimize-btn").click(function () {
-  $(".notification-modal-class").toggleClass("d-none");
-});
-
-//on load minimize notification modal
-$("#minimize-btn").click();
-
-function toggleAnnouncementsModal() {
-  $("#notification-modal-dialog").toggleClass("d-none");
-}
-
-// Event Modal
-//close event modal
-function eventmodal() {
-  $("#eventmodal").toggleClass("d-none");
-}
-//btn click
-$("#events-modal-btn").click(function () {
-  eventmodal();
-});
-
-$(".tab-item").click(function () {
-  $("#eventmodal").addClass("d-none");
-  $("#notification-modal-dialog").addClass("d-none");
-});
-
-
-
-//slick caorousel
-$(document).ready(function () {
-  width = $(window).width();
-  if (width >= 992) {
-    $(".multiple-items").slick({
-      rows: 2,
-      slidesPerRow: 3,
-      infinite: true,
-      slidesToScroll: 1,
-      // autoplay: true,
-      // autoplaySpeed: 3000,
-    });
-  } else if (width <= 576) {
-    $(".multiple-items").slick({
-      infinite: true,
-      rows: 2,
-      slidesPerRow: 2,
-      slidesToScroll: 2,
-      // autoplay: true,
-      // autoplaySpeed: 3000,
-    });
-  } else if (width >= 768 && width > 576) {
-    $(".multiple-items").slick({
-      infinite: true,
-      rows: 2,
-      slidesPerRow: 3,
-      slidesToScroll: 2,
-      // autoplay: true,
-      // autoplaySpeed: 3000,
-    });
-  }
-});
-
-//on resizing window adjust slick carousel
-
-// window.onresize = function (event) {
-//     width = $(window).width()
-//     if (width >= 992) {
-//         $(".multiple-items").slick({
-//             infinite: true,
-//             slidesToShow: 3,
-//             slidesToScroll: 4,
-//             autoplay: true,
-//             autoplaySpeed: 2000,
-//         });
-
-//     } else if (width <= 576) {
-//         $(".multiple-items").slick({
-//             infinite: true,
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 2000,
-
-//         });
-//     } else if (width >= 768 && width > 576) {
-//         $(".multiple-items").slick({
-//             infinite: true,
-//             slidesToShow: 2,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 2000,
-
-//         });
-//     }
-// };
-function VirtualBooth() {
-  document.getElementById("virtual_booth").click();
-}
 
 // there are two buttons
 // the button u see with an emoji => dummy button
@@ -173,6 +70,80 @@ $("body").scroll(function () {
   }
 });
 
+//auditorium - virtualbooth
+function VirtualBooth() {
+  document.getElementById("virtual_booth").click();
+}
+
+//expo hall - slick caorousel
+$(document).ready(function () {
+  width = $(window).width();
+  if (width >= 992) {
+    $(".multiple-items").slick({
+      rows: 2,
+      slidesPerRow: 3,
+      infinite: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  } else if (width <= 576) {
+    $(".multiple-items").slick({
+      infinite: true,
+      rows: 2,
+      slidesPerRow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  } else if (width >= 768 && width > 576) {
+    $(".multiple-items").slick({
+      infinite: true,
+      rows: 2,
+      slidesPerRow: 3,
+      slidesToScroll: 2,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  }
+});
+
+
+//Business card modal
+function openBusinessCardModal() {
+  $("#businessCardModal").modal("show");
+}
+
+// Notification Modal
+$("#minimize-btn").click(function () {
+  $(".notification-modal-class").toggleClass("d-none");
+});
+
+//on load minimize notification modal
+$("#minimize-btn").click();
+
+function toggleAnnouncementsModal() {
+  $("#notification-modal-dialog").toggleClass("d-none");
+}
+
+// Event Modal
+//close event modal
+function eventmodal() {
+  $("#eventmodal").toggleClass("d-none");
+}
+//btn click
+$("#events-modal-btn").click(function () {
+  eventmodal();
+});
+
+// on tab item click - close modal
+$(".tab-item").click(function () {
+  $("#eventmodal").addClass("d-none");
+  $("#notification-modal-dialog").addClass("d-none");
+});
+
+
+//chat Modal
 window.addEventListener("resize", function () {
   width = $(window).width()
   if (width <= 992) {
