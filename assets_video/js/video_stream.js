7df1bebc601831fa17dfc1522c63f02740b464e5
madhumithaa <madhumithaa1411@gmail.com>
2020-06-22 21:54:37 +0530

    video and lobby merged

---
+++ assets_video/js/video_stream.js
@@ -0,0 +1,46 @@
+toggleAnnouncementsModal();
+// Notification Modal
+$("#minimize-btn").click(function () {
+  $(".notification-modal-class").toggleClass("d-none");
+});
+
+//on load minimize notification modal
+$("#minimize-btn").click();
+
+function toggleAnnouncementsModal() {
+  $("#notification-modal-dialog").toggleClass("d-none");
+}
+
+window.addEventListener("resize", function () {
+  width = $(window).width();
+  if (width <= 992) {
+    $("#chatModal .close").click();
+  }
+});
+
+function display() {
+  $(".qa-btn").toggleClass("d-none");
+  $(".audience-btn").toggleClass("d-none");
+  $(".polling-btn").toggleClass("d-none");
+  $("#notification-btn").toggleClass("d-none");
+  $("#chat-modal-btn").toggleClass("d-none");
+}
+
+$(".audience-btn").click(function () {
+  $(".menu_icon").click();
+  $("#chat_button").click();
+});
+
+$(".qa-btn").click(function () {
+  $(".menu_icon").click();
+  $("#qa_button").click();
+});
+
+$(".polling-btn").click(function () {
+  $(".menu_icon").click();
+  $("#poll_button").click();
+});
+
+function btnclick() {
+  window.location.href = "./lobby_sarcon.html";
+}
